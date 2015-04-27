d411072239cc5b34fd02f59275b519f29eacd826
Robert <robfitz85@gmail.com>
2015-04-27 23:09:18 +0100

    Template layout of partials for site put in place with some layout concepts explored

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,5 +1,33 @@
-var myAppModule = angular.module('myAppModule', ['ui.router', 'ngResource', 'ngMaterial', 'ngMdIcons'])
-    .config( ['$httpProvider', '$urlRouterProvider', function($httpProvider, $urlRouterProvider) {
+var myAppModule = angular.module('myAppModule', ['ui.router', 'ngResource', 'ngMaterial', 'ngMdIcons','ngAnimate'])
+    .config( ['$httpProvider', '$urlRouterProvider','$stateProvider', function($httpProvider, $urlRouterProvider,$stateProvider) {
+    	
+    	$urlRouterProvider.otherwise("/home");
+    	
+    	$stateProvider.state('home', {
+    		url: '/home',
+    		templateUrl: 'partials/home.html',
+    		controller: myAppModule.gridListDemoCtrl
+    	})
+    	.state('about', {
+    		url: '/about',
+    		templateUrl: 'partials/about.html',
+    		controller: myAppModule.AboutController
+    	})
+    	.state('portfolio', {
+    		url: '/portfolio',
+    		templateUrl: 'partials/portfolio.html',
+    		controller: myAppModule.PortfolioController
+    	})
+    	.state('contact', {
+    		url: '/contact',
+    		templateUrl: 'partials/contact.html',
+    		controller: myAppModule.ContactController
+    	})
+    	.state('social', {
+    		url: '/social',
+    		templateUrl: 'partials/social.html',
+    		controller: myAppModule.SocialController
+          });
     	
     } ])
     .config(function($mdThemingProvider) {
