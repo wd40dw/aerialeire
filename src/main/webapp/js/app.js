c93b6d83d68b5d6b02f0026b2543187ba5226645
Robert <robfitz85@gmail.com>
2015-04-29 22:22:10 +0100

    Changed Materials library to materializecss instead of angular materials. Implemented parallax scrolling home page. Further work on structure/layout on site

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,4 @@
-var myAppModule = angular.module('myAppModule', ['ui.router', 'ngResource', 'ngMaterial', 'ngMdIcons','ngAnimate'])
+var myAppModule = angular.module('myAppModule', ['ui.router', 'ngResource', 'ngMdIcons','ngAnimate'])
     .config( ['$httpProvider', '$urlRouterProvider','$stateProvider', function($httpProvider, $urlRouterProvider,$stateProvider) {
     	
     	$urlRouterProvider.otherwise("/home");
@@ -6,7 +6,7 @@ var myAppModule = angular.module('myAppModule', ['ui.router', 'ngResource', 'ngM
     	$stateProvider.state('home', {
     		url: '/home',
     		templateUrl: 'partials/home.html',
-    		controller: myAppModule.gridListDemoCtrl
+    		controller: myAppModule.HomeController
     	})
     	.state('about', {
     		url: '/about',
@@ -29,40 +29,11 @@ var myAppModule = angular.module('myAppModule', ['ui.router', 'ngResource', 'ngM
     		controller: myAppModule.SocialController
           });
     	
-    } ])
-    .config(function($mdThemingProvider) {
-    	$mdThemingProvider.theme('default')
-        .primaryPalette('green', {
-          'default': '400',
-          'hue-1': '100',
-          'hue-2': '600',
-          'hue-3': 'A100'
-        })
-        .accentPalette('blue', {
-          'default': '200'
-        });
-    });
+} ]);
 
-myAppModule.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-  $scope.toggleLeft = buildToggler('left');
-  /**
-   * Build handler to open/close a SideNav; when animation finishes
-   * report completion in console
-   */
-  function buildToggler(navID) {
-    return function() {
-      return $mdSidenav(navID).toggle()
-        .then(function () {
-          $log.debug("toggle " + navID + " is done");
-        });
-    }
-  }
+$( document ).ready(function(){
+	
+	$('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
 })
-.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-  $scope.close = function () {
-    $mdSidenav('left').close()
-      .then(function () {
-        $log.debug("close LEFT is done");
-      });
-  };
-});
---
+++ src/main/webapp/js/controllers/home_controller.js
@@ -0,0 +1,4 @@
+myAppModule.controller('HomeController',function($scope, $http){
+    
+	$('.parallax').parallax();
+});
